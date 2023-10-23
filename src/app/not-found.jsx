a51319b8310f9356700c93cc8b8b6a664e90bc44
GustavoMG25 <RM97999@fiap.com.br>
2023-10-23 19:12:42 -0300

    criação do not-found-jsx

---
+++ src/app/not-found.jsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export default function NotFoun() {
+return (
+<>
+<h1> <strong> OOPS! 404 </strong></h1>
+<p> parece que você está tentando acessar uma página que não existe! </p>
+<p> <Link href="/"> Volte a página <strong>Home</strong> antes que você se perca!</Link></p>
+</>
+)
+}
