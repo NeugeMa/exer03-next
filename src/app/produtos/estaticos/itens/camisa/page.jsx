bd0c347a2ad29faee2c814022085a5d4dbba4d59
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-23 18:01:30 -0300

    apagando conteudo dinamico para se tornar estatico

--- src/app/produtos/estaticos/itens/camisa/page.jsx
+++ src/app/produtos/estaticos/itens/camisa/page.jsx
@@ -3,12 +3,10 @@ import Link from "next/link";
 
 export default function Camisas({params}) {
   return (
-    <div>
-        <h1>Madress Dados</h1>
-        <div>
-            <p>O valor é : {params.camisa}</p>
-            <p> <Link href="/"> Volte a página <strong>Home</strong></Link></p>
-        </div>
-    </div>
+<> 
+            <div>
+                    <p>O valor é : {params.camisas}</p>
+                </div>
+        </>
   ); 
 }
---
+++ src/app/produtos/estaticos/itens/oculos/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Oculos({params}) {
+    return (
+        <> 
+            <div>
+                    <p>O valor é : {params.oculos}</p>
+                </div>
+        </>
+    )
+    }
