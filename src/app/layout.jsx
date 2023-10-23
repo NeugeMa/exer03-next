0fd9745d841b60b461b3a6b2466e3cd46f6e3e5b
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-23 10:22:02 -0300

    arrumando código

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Cabecalho from "./components/cabecalho/cabecalho";
-import Rodape from "./components/rodape/rodape";
+import Cabecalho from "../components/cabecalho/cabecalho";
+import Rodape from "../components/rodape/rodape";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>
+
         <Cabecalho/>
         {children}
         <Rodape/>
