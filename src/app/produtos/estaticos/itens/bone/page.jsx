d4d3fa9947dae0eaeb1ac12a2b7f2cc923fc72b9
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-23 19:16:11 -0300

    retirando imports + page bone

---
+++ src/app/produtos/estaticos/itens/bone/page.jsx
@@ -0,0 +1,9 @@
+export default function Bone({params}) {
+    return (
+        <> 
+            <div>
+                    <p>O valor é : {params.bone}</p>
+                </div>
+        </>
+    )
+    }
