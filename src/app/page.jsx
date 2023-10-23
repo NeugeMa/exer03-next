6f9aa7c1458b1bc600fa80e6f82376e3b62acfad
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-23 09:10:15 -0300

    dentro da pages adicionamos conteudo sobre o exer + membros

---
+++ src/app/page.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>HOME 🌐</h1>
+        <p> <strong> Exercício03 - React + Next.js </strong></p>
+          <br/>
+        
+          <p> 0.1 Crie uma rota chamada produtos. <br/>  
+              0.2 Adicione um produto de nome item.(Faça o teste com os seguintes parâmetros camisa,
+                  calça, sapato, boné e óculos). <br/> 
+              0.3 Crie um único card. <br/>
+                0.03 No página quando receber o parâmetro, dependendo de qual parâmetro chegar,
+                carregue neste card a imagem referente ao produto passado. 
+          </p>
+
+          <main>
+            <br/> 
+                <h3><strong> Membranas</strong></h3>
+                  <p> </p>
+
+
+          </main>
+
+      
+
+    </>
+  )
+}
