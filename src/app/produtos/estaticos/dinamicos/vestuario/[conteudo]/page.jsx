cb790ffbd4b8c264640d0d04a188e39e993eb8f8
felipemoraes <rm98455@fiap.com.br>
2023-10-23 22:06:08 -0300

    criação das rotas dinâmicas + links

---
+++ src/app/produtos/estaticos/dinamicos/vestuario/[conteudo]/page.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Conteudo({ params }) {
+  return (
+    <>
+      <h1>
+        {" "}
+        <strong> MaDress</strong>
+      </h1>
+      <div>
+        <p>O valor é : {params.conteudo}</p>
+      </div>
+      <p>
+        {" "}
+        <Link href="/">
+          {" "}
+          Volte a página <strong>Home</strong>
+        </Link>
+      </p>
+      <figure>
+        <Image
+          src="/img/risos.png"
+          alt="Imagem sorrindo"
+          width={500}
+          height={500}
+        />
+      </figure>
+    </>
+  );
+}
