726988a1686e68a160e2b3500520e88c60671dad
NeugeMa <mariana.n.dourado@gmail.com>
2023-10-23 22:08:18 -0300

    adicionando mais itens ao cabecalho

--- src/components/cabecalho/cabecalho.jsx
+++ src/components/cabecalho/cabecalho.jsx
@@ -4,10 +4,11 @@ export default function Cabecalho() {
   return (
         <header className="bg-pink-200 h-18">
           <nav className="flex flex-row justify-around">
-            <Link className="link-cabecalho" href="/produtos/dinamicos/vestuario/tenis-rosa">Sapatos</Link>
-            <Link className="link-cabecalho" href="/produtos/dinamicos/vestuario/camisa-fiap">Vestuário</Link>
-            <Link className="link-cabecalho" href="/produtos/dinamicos/vestuario/oculos-virtual">Acessórios</Link>
-            <Link className="link-cabecalho" href="/produtos/dinamicos/vestuario/bone-verde">Itens Cabeça</Link>
+            <Link className="link-cabecalho" href="/">Sapatos</Link>
+            <Link className="link-cabecalho" href="/">Camisetas</Link>
+            <Link className="link-cabecalho" href="/">Carsolas</Link>
+            <Link className="link-cabecalho" href="/">Acessórios</Link>
+            <Link className="link-cabecalho" href="/">Itens Cabeça</Link>
           </nav>
         </header>
   )
